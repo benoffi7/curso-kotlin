5019423a7f0fe35d708f5485d3476a8177f91435
Gonzalo Benoffi <benoffi11@gmail.com>
2020-07-28 18:44:42 -0300

    Stream #1
    Primera clase. Actividad y XML. Internalización

---
+++ app/src/main/java/com/coffeeandcookies/cursokotlin/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.coffeeandcookies.cursokotlin
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        //busco en el archivo R (recursos) uno del tipo ventana (layout) y que coincida con el nombre del archivo XML
+        //establecemos la relación entre Kt con un XML
+        setContentView(R.layout.activity_main)
+        buttonAceptar.setOnClickListener {
+            revisarCajatexto()
+        }
+
+
+    }
+
+    fun revisarCajatexto()
+    {
+        if (editTextTextPersonName.text.isEmpty())
+        {
+            Toast.makeText(this,"No puede estar vacio!",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            //ir a otra pantalla
+        }
+    }
+}
--- app/src/main/java/com/coffeeandcookies/cursokotlin/Test.kt
+++ app/src/main/java/com/coffeeandcookies/cursokotlin/Test.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
-class MainActivity : AppCompatActivity()
+class Test : AppCompatActivity()
 {
 
     override fun onCreate(savedInstanceState: Bundle?) {
